18fd372c7692bd321da4be56abf6f011eb1dd894
lucas.ferreira <lucas.bref82@gmail.com>
2024-12-13 23:07:04 -0300

    Entidade e dados MOCK

---
+++ src/main/java/br/com/delivery/domain/entity/Restaurante.java
@@ -0,0 +1,32 @@
+package br.com.delivery.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "RESTAURANTES")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Restaurante {
+
+    @Id
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NOME", length = 30)
+    private String nome;
+
+    @Column(name = "TAXA_FRETE", precision = 10, scale = 2)
+    private BigDecimal taxaFrete;
+
+}
