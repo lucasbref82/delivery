dfc8d99685dfa7bc7e04a22943cde27bbc626060
lucas.ferreira <lucas.bref82@gmail.com>
2024-12-17 22:04:31 -0300

    Wrapper responses in a one object

--- src/main/java/br/com/delivery/v1/controller/KitchenController.java
+++ src/main/java/br/com/delivery/v1/controller/KitchenController.java
@@ -1,5 +1,6 @@
 package br.com.delivery.v1.controller;
 
+import br.com.delivery.v1.domain.dto.GenericMessage;
 import br.com.delivery.v1.domain.entity.Kitchen;
 import br.com.delivery.v1.domain.exception.NotFoundException;
 import br.com.delivery.v1.service.KitchenService;
@@ -7,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,19 +20,54 @@ public class KitchenController {
     private final KitchenService kitchenService;
 
     @GetMapping()
-    public List<Kitchen> findAll() {
-        return kitchenService.findAll();
+    public ResponseEntity<GenericMessage> findAll() {
+        try {
+            return ResponseEntity.ok(
+                    GenericMessage
+                            .builder()
+                            .sucess(true)
+                            .result(kitchenService.findAll())
+                            .build());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(
+                            GenericMessage
+                                    .builder()
+                                    .sucess(false)
+                                    .message(e.getMessage())
+                                    .build()
+                    );
+        }
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) {
+    public ResponseEntity<GenericMessage> findById(@PathVariable Long id) {
         try {
-            return ResponseEntity.ok(kitchenService.findById(id));
+            return ResponseEntity.ok(
+                    GenericMessage
+                            .builder()
+                            .sucess(true)
+                            .result(kitchenService.findById(id))
+                            .build());
         } catch (RuntimeException e) {
             if (e.getCause() instanceof NotFoundException) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .body(GenericMessage
+                                .builder()
+                                .sucess(false)
+                                .message(e.getMessage())
+                                .build());
             }
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(GenericMessage
+                            .builder()
+                            .sucess(false)
+                            .message(e.getMessage())
+                            .build());
         }
     }
 }
---
+++ src/main/java/br/com/delivery/v1/domain/dto/GenericMessage.java
@@ -0,0 +1,16 @@
+package br.com.delivery.v1.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenericMessage {
+    private boolean sucess;
+    private Object message;
+    private Object result;
+}
