fcb40d2c4a33597624d9c601a5cd9f2469b6d90b
lucas.ferreira <lucas.bref82@gmail.com>
2024-12-23 13:45:36 -0300

    Ajusdment city reposiry impl

--- src/main/java/br/com/delivery/v1/infrastructure/repositoryimpl/CityRepositoryImpl.java
+++ src/main/java/br/com/delivery/v1/infrastructure/repositoryimpl/CityRepositoryImpl.java
@@ -1,7 +1,9 @@
 package br.com.delivery.v1.infrastructure.repositoryimpl;
 
 import br.com.delivery.v1.domain.entity.City;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CityRepositoryImpl extends BaseRepositoryImpl<City, Long> {
     public CityRepositoryImpl() {
         super(City.class);
