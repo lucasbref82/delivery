ccd9dc8933efa41c8489f47c633da36291e144fe
lucas.ferreira <lucas.bref82@gmail.com>
2024-12-25 17:16:15 -0300

    Ajust many to many

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,35 +1,35 @@
-INSERT INTO kitchens (name) VALUES ('Tailandesa');
-INSERT INTO kitchens (name) VALUES ('Indiana');
-INSERT INTO kitchens (name) VALUES ('Brasileira');
-INSERT INTO kitchens (name) VALUES ('Japonesa');
+INSERT INTO kitchen (name) VALUES ('Tailandesa');
+INSERT INTO kitchen (name) VALUES ('Indiana');
+INSERT INTO kitchen (name) VALUES ('Brasileira');
+INSERT INTO kitchen (name) VALUES ('Japonesa');
 
-INSERT INTO payment_methods (description) values ("Pix.");
-INSERT INTO payment_methods (description) values ("Credit Card.");
-INSERT INTO payment_methods (description) values ("Debit Card.");
-INSERT INTO payment_methods (description) values ("Money.");
+INSERT INTO payment_method (description) values ("Pix.");
+INSERT INTO payment_method (description) values ("Credit Card.");
+INSERT INTO payment_method (description) values ("Debit Card.");
+INSERT INTO payment_method (description) values ("Money.");
 
-INSERT INTO restaurants (name, shipping_fee, kitchen_id) VALUES ("Pad Thai", 19.99, 1);
-INSERT INTO restaurants (name, shipping_fee, kitchen_id) VALUES ('Sabor Samosa', 25.65, 2);
-INSERT INTO restaurants (name, shipping_fee, kitchen_id) VALUES ('Coco Bambu', 50.00, 3);
-INSERT INTO restaurants (name, shipping_fee, kitchen_id) VALUES ('Sushi Sakura', 100.00, 4);
-
-
-INSERT INTO permissions (name, description) values ("READ_ONLY", "User has read only permission");
-INSERT INTO permissions (name, description) values ("READ_WRITE", "User has read and write permissions");
-INSERT INTO permissions (name, description) values ("ADMIN", "User has all permissions");
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ("Pad Thai", 19.99, 1);
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Sabor Samosa', 25.65, 2);
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Coco Bambu', 50.00, 3);
+INSERT INTO restaurant (name, shipping_fee, kitchen_id) VALUES ('Sushi Sakura', 100.00, 4);
 
 
-INSERT INTO states (name) values ("Minas Gerais.");
-INSERT INTO states (name) values ("Goiás.");
-INSERT INTO states (name) values ("Rio Grande do Norte.");
-INSERT INTO states (name) values ("Alagoas.")
+INSERT INTO permission (name, description) values ("READ_ONLY", "User has read only permission");
+INSERT INTO permission (name, description) values ("READ_WRITE", "User has read and write permissions");
+INSERT INTO permission (name, description) values ("ADMIN", "User has all permissions");
 
-INSERT INTO cities (name, state_id) values ("Nova Serrana.", 1);
-INSERT INTO cities (name, state_id) values ("Goiânia", 2);
-INSERT INTO cities (name, state_id) values ("Natal.", 3);
-INSERT INTO cities (name, state_id) values ("Maceió", 4);
 
-insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
+INSERT INTO state (name) values ("Minas Gerais.");
+INSERT INTO state (name) values ("Goiás.");
+INSERT INTO state (name) values ("Rio Grande do Norte.");
+INSERT INTO state (name) values ("Alagoas.")
+
+INSERT INTO city (name, state_id) values ("Nova Serrana.", 1);
+INSERT INTO city (name, state_id) values ("Goiânia", 2);
+INSERT INTO city (name, state_id) values ("Natal.", 3);
+INSERT INTO city (name, state_id) values ("Maceió", 4);
+
+insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
 
 
 
